6f4978432d0e7681a2ae1d2aa1cfdb49d7594bf3
유흥국 <soccer90200@naver.com>
2023-04-29 15:49:20 +0000

    FEATURE: 모델 생성

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Result, Question, Choice, Spot, Survey
 
-# Register your models here.
+admin.site.register(Result)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Spot)
+admin.site.register(Survey)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Result(models.Model):
+    name = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+    
+class Question(models.Model):
+    num = models.IntegerField(unique=True)
+    content = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.num}. {self.content}'
+    
+class Choice(models.Model):
+    content = models.CharField(max_length=100)
+    question_id = models.ForeignKey(to='main.Question', on_delete=models.CASCADE)
+    result_id = models.ForeignKey(to='main.Result', on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.content
+    
+class Spot(models.Model):
+    name = models.CharField(max_length=50)
+    spot_id = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    result_id = models.ForeignKey(to='main.Result', on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return f'{self.name} - {self.content}'
+    
+class Survey(models.Model):
+    accuracy = models.IntegerField(default=0)
+    satisfaction = models.IntegerField(default=0)
+    influence = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f'{self.accuracy}\n{self.satisfaction}\n{self.influence}'
+    

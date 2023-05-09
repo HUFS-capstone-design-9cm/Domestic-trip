e73d184c62f0271094443e0bf84e73b0dd8e7bfe
유흥국 <soccer90200@naver.com>
2023-05-09 19:14:34 +0000

    FEATURE: front-back merge

--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import Result, Question, Choice, Spot, Survey
+from .models import Traveler, Question, Choice, Spot, Survey
 
 def index(request):
-    results = Result.objects.all()
+    travelers = Traveler.objects.all()
     
     context = {
-        'results' : results,
+        'travelers' : travelers,
     }
     
     return render(request, 'index.html', context=context)
@@ -24,20 +24,25 @@ def result(request):
     N = Question.objects.count()
     # 여행자 유형 수
     K = Traveler.objects.count()
+    # 성격 유형
+    person = ''
     
     print(f'문항 수: {N}, 여행자 유형 수: {K}')
     
-    counter = [0] * (K+1)
+    counter = {'E': 0, 'I': 0, 'B': 0, 'R': 0, 'P': 0, 'J':0}
     
     print(f'POST: {request.POST}')
     
+    
     for n in range(1, N+1):
-        traveler_id = int(request.POST[f'question-{n}'][0])
-        counter[traveler_id] += 1
+        category = request.POST[f'question-{n}'][0]
+        counter[category] += 1
         
     # 최고점 여행자 유형
-    best_traveler_cnt = max(range(1, K+1), key=lambda id : counter[id])
-    best_traveler = Traveler.objects.get(pk=best_traveler_cnt)
+    for c in counter.keys():
+        if counter[c] >= 2:
+            person += c
+    best_traveler = Traveler.objects.get(personality=person)
     best_traveler.count += 1
     best_traveler.save()
     
@@ -46,4 +51,4 @@ def result(request):
         'counter' : counter
     }
     
-    return render(request, 'result.html', context)
+    return render(request, 'result.html', context=context)

6591496207dec6c173bf00b8bbfc2b143553f68c
유흥국 <soccer90200@naver.com>
2023-05-30 11:42:40 +0000

    FEATURE: 좌표 입력 및 만족도 API업데이트

--- main/models.py
+++ main/models.py
@@ -28,6 +28,7 @@ class Survey(models.Model):
     accuracy = models.IntegerField(default=0)
     satisfaction = models.IntegerField(default=0)
     influence = models.IntegerField(default=0)
+    count = models.IntegerField(default=0)
     
     def __str__(self):
         return f'{self.accuracy}\n{self.satisfaction}\n{self.influence}'
--- main/views.py
+++ main/views.py
@@ -3,44 +3,53 @@ from haversine import haversine
 from .models import Traveler, Question, Choice, Survey
 import sys, json
 
+
 def index(request):
-    travelers = Traveler.objects.all()
-    count = 0
+    survey = Survey.objects.get(pk=1)
     
-    for traveler in travelers:
-        count += traveler.count
+    if request.method == 'POST':
+        accuracy, satisfaction, influence = request.POST['accuracy'], request.POST['satisfaction'], request.POST['influence']
+        survey.accuracy += int(accuracy)
+        survey.satisfaction += int(satisfaction)
+        survey.influence += int(influence)
+        survey.save()
+        
+    travelers = Traveler.objects.all()
     
     context = {
         'travelers' : travelers,
-        'count' : count
+        'count' : survey.count
     }
-    
+
     return render(request, 'index.html', context=context)
+
 
 def form(request):
     questions = Question.objects.all()
     context = {
         'questions' : questions
     }
-    
+
     return render(request, 'form.html', context=context)
+
 
 def submit(request):
     # 문항 수
     N = Question.objects.count()
     # 여행자 유형 수
     travelers = Traveler.objects.all()
+    survey = Survey.objects.get(pk=1)
     # 성격 유형
     person = ''
-    
+
     counter = {'E': 0, 'I': 0, 'B': 0, 'R': 0, 'P': 0, 'J':0}
-    
+
     print(f'POST: {request.POST}')
 
     for n in range(1, N+1):
         category = request.POST[f'question-{n}'][0]
         counter[category] += 1
-        
+
     # 최고점 여행자 유형
     for c in counter.keys():
         if counter[c] >= 2:
@@ -49,9 +58,9 @@ def submit(request):
     for traveler in travelers:
         if traveler.data["personality"] == person:
             best_traveler = traveler
-    best_traveler.count += 1
-    best_traveler.save()
-
+    survey.count += 1
+    survey.save()
+    
     context = {
         'traveler' : best_traveler,
         'counter' : counter
@@ -67,7 +76,7 @@ def result(request, traveler_id):
         'traveler' : traveler,
         'traveler_id' : traveler_id,
     }
-    
+
     return render(request, 'result.html', context=context)
 
 
@@ -79,22 +88,14 @@ def search(request):
         spot.replace('[', '').replace(']', '').split(', ')
         for spot in selected_spots
     ]
-    # print(selected_spot)
-    
+
     places = {
-        spot[0]:list(map(float, [spot[1], spot[2]])) 
+        spot[2]: list(map(float, [spot[3], spot[4]]))
         for spot in selected_spot
     }
-    
-    places['start'] = [34.5709, 126.6079] # 시작 지점 수정 필요
-    # places = {
-    #     'start': [34.5709, 126.6079],
-    #     '전망대': [34.2961, 126.5246],
-    #     '미황사': [34.3829, 126.5774],
-    #     '대흥사': [34.4764, 126.6168],
-    #     '해창주조장': [34.5177, 126.5386],
-    #     '해남공룡박물관': [34.5897, 126.4375]
-    # }
+
+    places['start'] = list(map(float, selected_spot[0][:2]))# 시작 지점 수정 필요
+    print(places)
     
     regions = list(places.keys())
     NUM = len(regions)
@@ -108,7 +109,7 @@ def search(request):
                         'lng': str(lng)}
     }
 
-    while len(regions) != 0:       
+    while len(regions) != 0:
         for region in regions:
             temp = haversine(places[departure],
                             places[region],
@@ -116,52 +117,28 @@ def search(request):
             if distance > temp:
                 distance = temp
                 ans = region
-                
+
         departure = ans
         regions.remove(ans)
         sequence = NUM - len(regions)
         distance = sys.maxsize
         lat, lng = places[ans][0], places[ans][1]
         routes[ans] = {'sequence': sequence,
-                       'lat': str(lat), 
+                       'lat': str(lat),
                        'lng': str(lng)}
-        
-        
+
     context = {
         'routes': routes,
         'route_js': json.dumps(routes),
         'selected_spots': selected_spots,
     }
-    """
-    routes = {
-               'start': {'sequence': 1, 
-                          'lat': 34.5709, 
-                          'lng': 126.6079}, 
-                '해남공룡박물관': {'sequence': 2, 
-                                 'lat': 34.5897, 
-                                 'lng': 126.4375},
-                '해창주조장': {'sequence': 3, 
-                             'lat': 34.5177,
-                             'lng': 126.5386 },
-                '대흥사': {'sequence': 4, 
-                          'lat': 34.4764, 
-                          'lng': 126.6168}, 
-                '미흥사': {'sequence': 5, 
-                          'lat': 34.3829, 
-                          'lng': 126.5774}, 
-                '전망대': {'sequence': 6, 
-                          'lat': 34.2961, 
-                          'lng': 126.5246}
-            }
-    """
     return render(request, 'search.html', context=context)
-    print(context)
 
 
 def types(request):
     traveler = Traveler.objects.all()
     context = {
-        'traveler' : traveler,
+        'traveler': traveler,
     }
     return render(request, 'types.html', context=context)
 

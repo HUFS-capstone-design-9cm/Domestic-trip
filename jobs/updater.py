deb5c25d4a665f1c6149c33b8f9678e2a627e377
유흥국 <soccer90200@naver.com>
2023-06-09 03:40:11 +0000

    update

--- jobs/updater.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import scheduler_api
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(scheduler_api, 'cron', second=10)
-    # scheduler.add_job(scheduler_api, 'interval', seconds=1)
-    scheduler.start()

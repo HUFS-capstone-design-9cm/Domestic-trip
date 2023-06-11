50a5ecb65491c6b32aa2c27adc8ba8cec03dc9c8
유흥국 <soccer90200@naver.com>
2023-06-11 15:18:23 +0000

    평균 구하기 기능 추가

--- main/models.py
+++ main/models.py
@@ -5,10 +5,10 @@ class Traveler(models.Model):
     region = models.CharField(max_length=50)
     count = models.IntegerField(default=0)
     data = models.JSONField()
-    
+
     def __str__(self):
         return self.name
-    
+
     def show_survey(self):
         results = ['accuracy', 'satisfaction', 'influence']
         survey = self.data["survey"]
@@ -21,19 +21,38 @@ class Traveler(models.Model):
             else:
                 print(f'- {result}: 목표 충족')
         print('----------------------')
-    
+
+    def show_result(self):
+        travelers = Traveler.objects.all()
+        accuracy = 0
+        satisfaction = 0
+        influence = 0
+        count = 0
+        for traveler in travelers:
+            count += traveler.count
+            survey = traveler.data["survey"]
+            accuracy += survey['accuracy'].count(1) + survey['accuracy'].count(2)
+            satisfaction += survey['satisfaction'].count(1) + survey['satisfaction'].count(2)
+            influence += survey['influence'].count(1) + survey['influence'].count(2)
+
+        print(accuracy, accuracy/count)
+        print(satisfaction, satisfaction/count)
+        print(influence, influence/count)
+
+
 class Question(models.Model):
     num = models.IntegerField(unique=True)
     data = models.JSONField()
-    
+
     def __str__(self):
         return self.num
-    
+
+
 class Choice(models.Model):
     content = models.CharField(max_length=100)
     question_id = models.ForeignKey(to='main.Question', on_delete=models.CASCADE)
     category = models.CharField(max_length=1)
-    
+
     def __str__(self):
         return self.content
 
